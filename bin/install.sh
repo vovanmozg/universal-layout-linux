fb2f9167cb017dc20794ec778529849051e236a3
vovanmozg <vovanmozg@gmail.com>
2023-02-28 09:18:16 +0400

    Add evdev.xml backuping

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,7 @@
 
 # run as sudo
 
+cp /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/evdev.xml.bak
 cp ./config/rules/evdev.xml /usr/share/X11/xkb/rules/
 cp ./config/symbols/ru-dev /usr/share/X11/xkb/symbols/
 cp ./config/symbols/us-dev /usr/share/X11/xkb/symbols/
